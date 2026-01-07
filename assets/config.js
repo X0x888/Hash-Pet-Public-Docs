bb2954d3ccc666d4b6e93716689e0d3f25d5ac64
X0x888 <test@mail.get-rich.uk>
2026-01-07 16:05:57 +0000

    added websites

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,5 @@
 window.HASHPET_SITE_CONFIG = {
   feedbackFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSeLpNytgbMkGkpSgd9FAE_Uz_uZ4OnO8W_PwwQ9w_Of2sboBg/viewform?usp=publish-editor",
-  githubRepoUrl: "https://github.com/YOUR_GITHUB_USERNAME/HashPet"
+  githubRepoUrl: "https://
+  github.com/x0x888/Hash-Pet-Public-Docs"
 };
